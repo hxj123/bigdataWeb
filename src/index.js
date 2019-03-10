a87681285b34403bad1d16e3eb89099962352463
hxj123 <1317490530@qq.com>
2019-03-10 09:41:54 +0800

    'a'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add("hello");
+    console.log(1)
     return element;
   }
   
